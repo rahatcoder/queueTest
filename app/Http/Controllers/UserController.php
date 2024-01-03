eef3be2d6e3c8fa4084258af3638c5f660e726d8
Ashfaq <ashfaq.uylab@gmail.com>
2024-01-03 18:06:43 +0600

    ControllerMake

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,35 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        return view('admin.user.users.all');
+    }
+
+    public function create(){
+        return view();
+    }
+
+    public function view(){
+        return view();
+    }
+
+    public function store(){
+        return view();
+    }
+
+    public function edit(){
+        return view();
+    }
+
+    public function update(){
+        return view();
+    }
+
+    public function distroy(){
+        return view();
+    }
 }
