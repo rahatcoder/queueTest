3366fb57b55d3c5069755f8043b41be0c845a2e5
Ashfaq <ashfaq.uylab@gmail.com>
2024-01-04 16:16:13 +0600

    AddDash

--- resources/views/admin/user/users/all.blade.php
+++ resources/views/admin/user/users/all.blade.php
@@ -264,36 +264,55 @@
 			</nav>
 			<section>
 				<div class="container">
+				<div class="row" style="margin-top:50px">
+				<div class="col-md-1"></div>
+				<div class="col-md-7">
+					<div class="row">
+						<h3>All Users</h3>
+					</div>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-2" style="margin-left:30px";>
+				<ul>
+					<li class="btn btn-dark";><a href="user/addUsers">ADD USER</a></li>
+				</ul>
+				<div class="col-md-1"></div>
+				</div>
+				</div>
+			</section>
+			<section>
+				<div class="container">
 					<div class="row">
 						<div class="col-md-1"></div>
 						<div class="col-md-10">
-						<table class="table table-dark table-striped" style="margin-top:50px";>
+						<table class="table table-dark table-striped" style="margin-top:20px";>
 							<thead class="th">
     							<tr>
       							<th scope="col">ID</th>
-      							<th scope="col">Name</th>
-      							<th scope="col">Price</th>
-      							<th scope="col">Quantity</th>
+      							<th scope="col">User Name</th>
+      							<th scope="col">Email</th>
+      							<th scope="col">Manage</th>
     							</tr>
   							</thead>
   							<tbody>
+								@foreach($users as $data)
     							<tr>
-      							<th scope="row">1</th>
-      							<td>Mark</td>
-      							<td>Otto</td>
-      							<td>@mdo</td>
+      							<th scope="row">{{$data->id}}</th>
+      							<td>{{$data->name}}</td>
+      							<td>{{$data->email}}</td>
+      							<td>
+									<div class="dropdown">
+  									<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Manage
+  									</button>
+  										<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    										<li><a class="dropdown-item" href="#">Show</a></li>
+											<li><a class="dropdown-item" href="#">Modify</a></li>
+    										<li><a class="dropdown-item" href="#">Destroy</a></li>
+  										</ul>
+									</div>
+								</td>
     							</tr>
-    							<tr>
-      							<th scope="row">2</th>
-      							<td>Jacob</td>
-      							<td>Thornton</td>
-      							<td>@fat</td>
-    							</tr>
-    							<tr>
-      							<th scope="row">3</th>
-      							<td colspan="2">Larry the Bird</td>
-      							<td>@twitter</td>
-    							</tr>
+    							@endforeach
   							</tbody>
 
 						</table>
